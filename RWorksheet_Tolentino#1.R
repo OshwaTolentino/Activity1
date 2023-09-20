7d0024646a73105a10f8ca678b454fc134de2c41
OshwaTolentino <aaronjoshuatolentino055@gmail.com>
2023-09-20 08:04:24 +0000

    Hello

---
+++ RWorksheet_Tolentino#1.R
@@ -0,0 +1,48 @@
+age <-c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41, 51, 35, 24, 33, 41)
+age
+
+
+#1. The data point is 34
+length(age)
+
+#2. 
+
+
+#3. 
+new_age <-c(age,0,age)
+new_age
+
+#4.
+sort(age)
+
+#5. Minimum and Maximum
+min(age)
+max(age)
+
+#6. 
+data <-c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+         2.5, 2.3, 2.4, 2.7)
+
+#There are 12 data points
+length(data)
+
+
+#7.
+data_doubled <- data*2
+data_doubled
+
+#8.1
+seq(1:100)
+
+#8.2
+seq(20,60)
+
+#8.3
+mean(20:60)
+
+#8.4
+sum(51:91)
+
+#8.5
+seq(1:1000)
+
